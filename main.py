dda7cba51de703003af8e105afb7684853678f58
zahrahijazii <105450781+zahrahijazii@users.noreply.github.com>
2023-08-03 17:49:25 +0400

    done creating the two paddles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from paddle import Paddle
 
 
 screen = Screen()
@@ -7,9 +8,23 @@ screen = Screen()
 screen.title("Ping Pong")
 screen.bgcolor("black")
 screen.setup(800, 600)
+screen.tracer(0)
+
+#set up the paddle:
+r_paddle = Paddle((350, 0)) 
+l_paddle = Paddle((-350, 0)) 
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 
+# set up the movements of the paddle
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 
-
-
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 screen.exitonclick()
