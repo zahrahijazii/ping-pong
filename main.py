a7d034f5693ebacf34a02047498f24b4ec5b884f
zahrahijazii <105450781+zahrahijazii@users.noreply.github.com>
2023-08-03 17:30:27 +0400

    set up the main screen

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+
+screen = Screen()
+
+#set up the main screen
+screen.title("Ping Pong")
+screen.bgcolor("black")
+screen.setup(800, 600)
+
+
+
+
+
+screen.exitonclick()
