f02ad498427728e3fad4cd10896a270fc51586d0
zahrahijazii <105450781+zahrahijazii@users.noreply.github.com>
2023-08-04 20:48:54 +0400

    detect collision with paddles

--- ball.py
+++ ball.py
@@ -11,6 +11,9 @@ class Ball(Turtle):
     def move(self):
         self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
     
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+    
+    def bounce_x(self):
+        self.x_move *= -1
         
--- main.py
+++ main.py
@@ -17,6 +17,15 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0)) 
 l_paddle = Paddle((-350, 0)) 
 
+
+# set up the movements of the paddle
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
 #set up the ball:
 ball = Ball()
 
@@ -28,17 +37,16 @@ while game_is_on:
     ball.move()
     #Detect collision with wall:
     if ball.ycor() > 280 or ball.ycor() < -280 :
-        ball.bounce()
+        ball.bounce_y()
+    
+    #Detect collision with paddle:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320 :
+        ball.bounce_x()
+
     
  
 
 
 
-# set up the movements of the paddle
-screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
 
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
 screen.exitonclick()
